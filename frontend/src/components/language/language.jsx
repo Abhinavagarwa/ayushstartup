7c093c4dac52a61d23ea176c2b5be55189f0d2b2
shreyajgit <j23.shreya@gmail.com>
2024-09-09 20:41:57 +0530

    Created application form , fixed navbar ,and language btn , all for both dark and light mode

--- frontend/src/components/language/language.jsx
+++ frontend/src/components/language/language.jsx
@@ -1,13 +1,13 @@
-import './language.css'
+import './language.css';
 
-const Languageoption=(props)=>{
-    return(
-        <select onChange={props.onChange} className='translate'>
-            <option >Translate</option>
+const Languageoption = (props) => {
+    return (
+        <select onChange={props.onChange} className="translate">
             <option value={'en'}>English</option>
             <option value={'hi'}>Hindi</option>
-            <option value={'chi'}>chinese</option>
+            <option value={'chi'}>Chinese</option>
         </select>
-    )
-}
+    );
+};
+
 export default Languageoption;
